80affc469aade8cc1b090074dc4fc6e76276435d
Ventsislav Dimitrov <ventsi34@gmail.com>
2014-12-11 21:14:01 +0200

    Change

--- bn-api/app/configs/dbConfig.php
+++ bn-api/app/configs/dbConfig.php
@@ -1,5 +1,5 @@
 <?php
 define('__DB_HOST__', 'localhost');
 define('__DB_NAME__', 'ventsii_birthday_notifier_test');
-define('__DB_USERNAME__', 'ventsii');
-define('__DB_PASSWORD__', 'dvamataglupaci');
+define('__DB_USERNAME__', '******');
+define('__DB_PASSWORD__', '******');
